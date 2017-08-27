283f54686e00b20e8977e3cc0762fd3647628234
Jan Moren <jan.moren@gmail.com>
2017-08-27 13:52:17 +0900

    add ruse_omp openmp test program

---
+++ util/ruse_omp.c
@@ -0,0 +1,75 @@
+/* ruse_omp.c 
+ *
+ * OpenMP threads with specified memory allocation.
+ *
+ * Run this with Ruse as:
+ * 
+ *     ruse -t1 -s --stdout ./ruse_omp
+ *
+ * You should see output similar to:
+ *
+ * time(s)   mem(MB)
+ * 0         1.5
+ * 1         11.4
+ * 2         13.4
+ * 3         21.4
+ * 4         21.4
+ * 5         21.4
+ * 6         11.6
+ * 7         11.6
+ * 
+ * Time(s):  7
+ * Mem(MB):  21.4
+ *  
+ *
+ * Copyright 2017 Jan Moren
+ *
+ * This file is part of Ruse.
+ *
+ * Ruse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ruse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ruse.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <error.h>
+#define KB 1024
+#define MB (KB*KB)
+
+/* Allocate and touch a set amount of memory */
+char *
+memalloc(int mb) {
+    char *mem = (char*)malloc(mb*MB);
+    for(int i=0; i<mb*MB; i++) {
+	mem[i] = 1;
+    } 
+    return mem;
+}
+
+int
+main(int argc, char*argv[]) {
+
+    /* Run the loop using two OMP threads (unless your machine has only 
+     * a single available core) 
+    */
+
+    #pragma omp parallel for
+    for (int i=0; i<2; i++) {
+	sleep(i*2);
+	char *mem = memalloc(10);
+	sleep(5);
+	free(mem);    
+    }
+}
