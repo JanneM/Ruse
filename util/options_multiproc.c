1d597764cf385ee6037062cc381ea61095e63cb5
Jan Moren <jan.moren@gmail.com>
2017-09-02 20:30:24 +0900

    add options processing for multiproc util

---
+++ util/options_multiproc.c
@@ -0,0 +1,98 @@
+/* options_multiproc.c
+ *
+ * Parse user options for ruse_multiproc.c
+ *
+ * Copyright 2017 Jan Moren
+ *
+ * This file is part of Ruse.
+ *
+ * Ruse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ruse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ruse.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#define _GNU_SOURCE
+
+#include "options_multiproc.h"
+
+void 
+show_help(const char *progname) {
+    printf("Usage: %s [FLAGS] [--help]\n", progname);
+    printf("\
+Run a test program that forks [procs] subprocesses.\n\
+\n\
+  -p, --procs=PROCS      Fork into PROCS subprocesses.\n\
+  -t, --time=SECONDS     Running time (5 seconds)\n\
+\n\
+      --help             Print help\n\
+\n");
+    exit(EXIT_SUCCESS);
+}
+
+
+options*
+get_options(int *argc, char **argv[]) {
+
+    options *opts = malloc(sizeof(options));
+    opts->procs   = 2;
+    opts->busy	  = true;
+    opts->time    = 5;
+
+    int c;
+
+    while (1) {
+	int option_index = 0;
+	static struct option long_options[] = {
+	    {"help",    no_argument,       0,  1 },
+	    {"procs",    no_argument,       0, 's'},
+	    {"time",    required_argument, 0, 't'},
+	    {0,         0,                 0,  0 }
+	};
+
+	c = getopt_long(*argc, *argv, "+ht:p:",
+		long_options, &option_index);
+	if (c == -1)
+	    break;
+	
+	switch (c) {
+	    
+	    case 1:
+		show_help((**argv));
+		exit(EXIT_SUCCESS);
+		break;
+	    
+	    case 't':
+		opts->time = atoi(optarg);
+		if (opts->time<1) {
+		    fprintf(stderr, "\nError: time must be a positive integer\n\n");
+		    show_help((**argv));
+		    exit(EXIT_FAILURE);
+		}
+		break;
+	    case 'p':
+		opts->procs = atoi(optarg);
+		if (opts->procs<1) {
+		    fprintf(stderr, "\nError: procs must be a positive integer\n\n");
+		    show_help((**argv));
+		    exit(EXIT_FAILURE);
+		}
+		break;
+	    case '?':
+	    default:
+		show_help((**argv));
+		exit(EXIT_FAILURE);
+		break;
+	}
+
+    }
+
+    return opts;
+}
---
+++ util/options_multiproc.h
@@ -0,0 +1,50 @@
+/* options_multiproc.h
+ *
+ * Parse user options for ruse_multiproc.c
+ *
+ * Copyright 2017 Jan Moren
+ *
+ * This file is part of Ruse.
+ *
+ * Ruse is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ruse is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ruse.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef OPTIONS_H
+#define OPTIONS_H
+#include <error.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "config.h"
+
+typedef struct {
+    unsigned int time;
+    bool busy;
+    unsigned int procs;
+} options;
+
+
+
+/* Get and parse user options. Return an options struct, and index to the first non-option argument in argv.
+ */
+
+options*
+get_options(int *argc, char **argv[]);
+
+#endif
