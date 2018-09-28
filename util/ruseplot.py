2d1bebe9f24b875872dfcceb1319b7715531228d
Jan Moren <jan.moren@gmail.com>
2018-09-28 01:06:04 -0400

    decided on ruseplot utility name

---
+++ util/ruseplot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# ruseplot
+#
+# A utility for plotting ruse-generated data.
+#
+# run ruse with the "-s" flag to generate time-stamped data. Then run:
+#
+# ruseplot.pl <ruse data file> to genrate a plot
+#
+
+import matplotlib as mp
+
+
+
